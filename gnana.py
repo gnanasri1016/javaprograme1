9584c621439d446f46b4bdd8cc512cf04042e0e0
Gnana sri <gnanasri1003@gmail.com>
2025-08-02 10:57:25 +0530

    add counselling form.html

--- gnana.py
+++ gnana.py
@@ -1,1 +1,11 @@
-print("Gnanasri is doing devops.")
+public class Add{
+public static int sum(int a,int b){
+return a+b;
+}
+public static void main(String[] args){
+int num1=5;
+int num2=8;
+int result=sum(num1,num2);
+System.out.println(num1+ "+" +num2 + "=" +result);
+}
+}
